8271502a485247a2870b410d222532fc7e4736e4
Hendrik H <hharlichs@googlemail.com>
2020-05-11 12:21:43 +0200

    clean text input after adding todo

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ todoButton.addEventListener("click", addTodo);
 
 let todoCount = 0;
 
-// Create todo html string with template literals
+// create todo html string with template literals
 const fetchedTodoString = (text) => `<li id="todo-list-item">${text}</li>`;
 
+// get input, run fetchedTodoString funtion and append todo using throwaway <div>
 function addTodo(event) {
-	event.preventDefault();
-	// console.log("Add todo", ++todoCount);
+	event.preventDefault(); // prevent page reload
 	const fetchedTodo = document.getElementById("todo-input").value;
-	// console.log("todo fetched:", fetchedTodo);
-
+	console.log("new todo", fetchedTodo, "added,", ++todoCount, "todos total"); // log number of todos
 	const div = document.createElement("div");
 	div.innerHTML = fetchedTodoString(fetchedTodo);
 	todoList.append(div.firstChild);
+	document.getElementById("todo-input").value = ""; // clear text input
 }
